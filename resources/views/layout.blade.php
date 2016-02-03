b9b45988e94013ecddc317bb2e6b3e9ca8fbe80a
naxjud <naxjud@yahoo.com>
2016-02-03 10:50:11 +0100

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Project Flyer</title>
+    <link rel="stylesheet" href="css/app.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+
+    <h1>Project Flyer</h1>
+
+  </body>
+</html>
